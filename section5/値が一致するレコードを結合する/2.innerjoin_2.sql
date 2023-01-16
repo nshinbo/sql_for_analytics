06e8596fe270acba91a454aa9b86d349c35c58ca
nshinbo <122785526+nshinbo@users.noreply.github.com>
2023-01-16 23:26:54 +0900

    orders2とitemsをINNERJOINする

---
+++ section5/値が一致するレコードを結合する/2.innerjoin_2.sql
@@ -0,0 +1,12 @@
+
+SELECT
+ order_id,
+ user_id,
+ price
+ orders.item_id,
+FROM
+ `sql-project-368204.section5.orders` AS orders2
+ INNER JOIN `sql-project-368204.section5.items` AS items
+ on orders.item_id = items.item_id 
+ 
+ 
