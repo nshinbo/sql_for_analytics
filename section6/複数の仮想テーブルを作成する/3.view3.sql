cb75a80a7f9f848d95bad22539cfc506c4dda7a3
nshinbo <122785526+nshinbo@users.noreply.github.com>
2023-01-17 10:25:33 +0900

    view2から平均購入額を取得するview3を作成する

---
+++ section6/複数の仮想テーブルを作成する/3.view3.sql
@@ -0,0 +1,38 @@
+
+WITH view1 AS (
+ SELECT 
+   items.item_id,
+   user_id,
+   price
+ FROM 
+   `sql-project-368204.section6.items` AS items 
+ INNER JOIN 
+   `sql-project-368204.section6.orders2`  AS orders
+   ON items.item_id = orders.item_id
+),
+
+view2 AS(
+  SELECT 
+    user_id, 
+    SUM(price) AS sum_price
+  FROM
+   view1 
+  GROUP BY
+   user_id 
+)
+
+view3 AS(
+  SELECT 
+    AVG(sum_price) AS avg_price
+  FROM view2
+)
+
+
+#view3の中身を確認
+SELECT
+ *
+FROM
+ view3
+ 
+ 
+ 
